5d4152064f562b817c158325f37fa922a3c6c01e
benitaibrahim <benita.ibrahim23@gmail.com>
2026-05-22 16:50:31 +0200

    Add files via upload

---
+++ play_video/play_video-2.py
@@ -0,0 +1,7 @@
+from video_visualiser import play_video
+
+
+folder = r"/home/ibrahim/Bureau/PTUT/Minipatches_light_20260116/20221012T125943_SmallPatches_C1-CAM5"
+type_video = "silhouettes" # This can be tierpsy, mkv, tif, or silhouettes
+
+play_video(folder, type_video=type_video)
